69644957b36f409cafdad40f496b9b233a25924a
Matt <matheusstolet@hotmail.com>
2019-11-07 18:24:55 -0800

    Clean different features from Berlin and Ravdess dataset

--- data_loading.py
+++ data_loading.py
@@ -2,48 +2,118 @@ import numpy as np
 import pandas
 import glob
 import os
+import librosa
+import re
+import pandas as pd
+import pdb
 
-ravdess_path = "data/ravdess/"
-berlin_path = "data/berlin/wav/"
+RAVDESS = "data/ravdess/"
+BERLIN = "data/berlin/wav/"
+AUDIO = 0
+SAMPLING_RATE = 1
+EMOTION = 2
+ACTOR = 3
+GENDER = 4
+
+# Possible Emotions
+#  - Neutral
+#  - Calm
+#  - Happy
+#  - Sad
+#  - Angry
+#  - Fearful
+#  - Disgust
+#  - Surprised
+#  - Boredom
+
+# Possible genders
+#  - male
+#  - female
+
+# 34 possible actors
 
 class Dataset():
-    def __init__(self, dataset_path):
-        self.data = []
-        self.dataset_path = dataset_path
-
-    def load(self):
-        # Counts number of files in directory
-        n = self._get_num_files(self.dataset_path)
-        self.data = np.array(n)
-        for filename in glob.iglob(self.dataset_path + "**", recursive=True):
+    def __init__(self):
+        self.data = np.array(0, dtype=object)
+        self.feature_names = ["audio", "sampling_rate", "emotion", "actor", "gender"]
+        
+    def load(self, path):
+        n = Dataset._get_num_files(path)
+        temp = np.zeros(n, dtype=object)
+        i = 0
+        for filename in glob.iglob(path + "**", recursive=True):
             if ".wav" in filename:
-                print(filename)
+                element = {"audio": None, "sampling_rate": None, "emotion": None, "actor": None, "gender": None}
 
-    def _get_num_files(self, path):
+                audio, sampling_rate = librosa.load(filename)
+                emotion = Dataset._get_emotion(filename, path)
+                actor = Dataset._get_actor(filename, path)
+                gender = Dataset._get_gender(filename, path)
+               
+                element["audio"] = audio
+                element["sampling_rate"] = sampling_rate
+                element["emotion"] = emotion
+                element["actor"] = actor
+                element["gender"] = gender
+                temp[i] = element
+                i += 1
+            if i == 5:
+                break;
+        self.data = np.append(self.data, temp)
+
+
+
+
+    @staticmethod
+    def _get_num_files(path):
         total = 0
         for root, dirs, files in os.walk(path):
             total += len(files)
         return total
+
+    @staticmethod
+    def _get_emotion(filename, dataset_type):
+        if dataset_type == RAVDESS:
+            return re.findall("[0-9][0-9]", filename)[2]
+        elif dataset_type == BERLIN:
+            emotion = re.search("[A-Z]", filename)
+            
+    @staticmethod
+    def _get_actor(filename, dataset_type):
+        if dataset_type == RAVDESS:
+            return re.findall("[0-9][0-9]", filename)[6]
+        elif dataset_type == BERLIN:
+            actor = re.search("[a-z][0-9][0-9]", filename)
+        
+    @staticmethod
+    def _get_gender(filename, dataset_type):
+        if dataset_type == RAVDESS:
+            actor = re.findall("[0-9][0-9]", filename)[6]
+            if int(actor) % 2 == 0:
+                return "female"
+            else:
+                return "male"
+        elif dataset_type == BERLIN:
+            actor = re.findall("[0-9][0-9]", filename)[0]
+            return Dataset._get_berlin_gender_from_actor(actor)
  
-class RavdessDataset(Dataset):
-    def __init__(self):
-        super(Dataset, self).__init__()
-
-    def load(self):
-        pass
-
-class BerlinDataset(Dataset):
-    def __init__(self):
-        super(Dataset, self).__init__()
-
-    def load(self):
-        pass
-
-
+    @staticmethod
+    def _get_berlin_gender_from_actor(actor):
+        switch = {
+                "03": "male",
+                "08": "female",
+                "09": "female",
+                "10": "male",
+                "11": "male",
+                "12": "male",
+                "13": "female",
+                "14": "female",
+                "15": "male",
+                "16": "female"
+        } 
+        return switch.get(actor, "Invalid actor!")
 
 if __name__ == "__main__":
-    print("Loading le data")
-    ravdess = Dataset(ravdess_path)
-    ravdess.load()
-    berlin = Dataset(berlin_path)
-    berlin.load()
+    dataset = Dataset()
+    dataset.load(RAVDESS)
+    dataset.load(BERLIN)
